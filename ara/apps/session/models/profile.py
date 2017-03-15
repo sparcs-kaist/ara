aeb957570c11be41cac666695a028c843e9f2fcd
Kang-in Raon Kim <talkwithraon@gmail.com>
2017-03-16 00:21:23 +0900

    UserProfile 모델 작성

---
+++ ara/apps/session/models/profile.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+from apps.core.classes.model import MetaInfoModel
+
+
+class UserProfile(MetaInfoModel):
+    class Meta:
+        verbose_name = '회원 정보'
+        verbose_name_plural = '회원 정보'
+
+    nick = models.CharField(
+        unique=True,
+        max_length=32,
+    )
+
+    # FK Fields
+    user = models.OneToOneField(
+        to='auth.User',
+        primary_key=True,
+        related_name='profile'
+    )
+
+    def __str__(self):
+        return '{nick}({user})'.format(
+            nick=self.nick,
+            user=self.user,
+        )
