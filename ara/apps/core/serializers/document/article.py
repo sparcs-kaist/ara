9ba51120921145cee607b923b7fd53c65fbb39a0
Kang-in Raon Kim <talkwithraon@gmail.com>
2017-03-22 14:31:21 +0900

    ArticleSerializer를 Request Methods 별로 분리
    - List/Retrieve 용의 ArticleSafeMethodSerializer, Create 용의 ArticleCreateMethodSerializer, Update/Partial Update용의 ArticleUpdateMethodSerializer로 분리
    - List/Retrieve 에서 보여주고, Create, Update, Partial Update에서 입력 할 수 있는 필드를 다르게 하기 위함임

--- ara/apps/core/serializers/document/article.py
+++ ara/apps/core/serializers/document/article.py
@@ -1,13 +1,44 @@
 from rest_framework import serializers
 
 from apps.core.models import Article
+from apps.core.serializers.category import CategorySerializer
 
 
-class ArticleSerializer(serializers.ModelSerializer):
+class ArticleSafeMethodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+        read_only_fields = (
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        )
+
+    categories = CategorySerializer(
+        many=True,
+    )
+
+
+class ArticleCreateMethodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         exclude = (
             'created_by',
+        )
+        read_only_fields = (
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        )
+
+
+class ArticleUpdateMethodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        exclude = (
+            'created_by',
+            'is_anonymous',
+            'use_signature',
         )
         read_only_fields = (
             'created_at',
--- ara/apps/core/views/viewsets/document/article.py
+++ ara/apps/core/views/viewsets/document/article.py
@@ -1,13 +1,25 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from apps.core.classes.mixin import DebugModeAuthMixin
 from apps.core.models import Article
-from apps.core.serializers.document.article import ArticleSerializer
+from apps.core.serializers.document.article import ArticleSafeMethodSerializer, ArticleCreateMethodSerializer, ArticleUpdateMethodSerializer
 
 
 class ArticleViewSet(DebugModeAuthMixin, viewsets.ModelViewSet):
     queryset = Article.objects.select_related('created_by').prefetch_related('comments', 'categories')
-    serializer_class = ArticleSerializer
+    safe_method_serializer_class = ArticleSafeMethodSerializer
+    unsafe_method_serializer_class = {
+        'create': ArticleCreateMethodSerializer,
+        'update': ArticleUpdateMethodSerializer,
+        'partial_update': ArticleUpdateMethodSerializer,
+    }
+
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return self.safe_method_serializer_class
+
+        else:
+            return self.unsafe_method_serializer_class[self.action]
 
     def perform_create(self, serializer):
         serializer.save(
