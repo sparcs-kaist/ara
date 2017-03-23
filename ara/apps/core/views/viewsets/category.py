0066af66593077bfd96640255ec69d4e7a21c616
Kang-in Raon Kim <talkwithraon@gmail.com>
2017-03-23 15:36:02 +0900

    CategoryFilterBackend 작성 및 ArticleViewSet 에 적용

--- ara/apps/core/views/viewsets/category.py
+++ ara/apps/core/views/viewsets/category.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 
+from apps.core.classes.mixin import DebugModeAuthMixin
 from apps.core.models import Category
 from apps.core.serializers.category import CategorySerializer
 
 
-class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+class CategoryViewSet(DebugModeAuthMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
